5bea2b1abf15b6ab1b92c6184c83440132b11450
physicstjc <85329791+physicstjc@users.noreply.github.com>
2024-09-12 14:13:39 +0800

    Update header-loader.js

--- header-loader.js
+++ header-loader.js
@@ -79,3 +79,16 @@ function toggleMenu() {
         menu.classList.toggle('responsive');
 }
 
+const accordions = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < accordions.length; i++) {
+    accordions[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        const panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
